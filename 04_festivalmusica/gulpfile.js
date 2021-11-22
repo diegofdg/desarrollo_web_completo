eafb0b68715a24e0f45ababe4ecdbf98847e7fda
diegofdg <72081847+diegofdg@users.noreply.github.com>
2021-11-22 13:36:15 -0300

    04_festivalmusica-mejorando código js con terser y gulp

--- 04_festivalmusica/gulpfile.js
+++ 04_festivalmusica/gulpfile.js
@@ -9,6 +9,7 @@ const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
+const terser = require('gulp-terser-js');
 
 function css( done ) {
     src('src/scss/**/*.scss')
@@ -54,6 +55,9 @@ function versionAvif( done ){
 
 function javascript( done ) {
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe( terser() )
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
     done();
 }
