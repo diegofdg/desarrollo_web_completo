b1c54cc9b68eab0eca6925779dad5d915d1ce1f5
diegofdg <72081847+diegofdg@users.noreply.github.com>
2021-11-26 08:15:06 -0300

    05_bienesraices-creando un footer

--- 05_bienesraices/base.php
+++ 05_bienesraices/base.php
@@ -3,16 +3,5 @@
     <main class="contenedor seccion">
         <h1>Base</h1>
     </main>
-    <footer class="footer seccion">
-        <nav class="navegacion">
-            <a href="nosotros.php">Nosotros</a>
-            <a href="anuncios.php">Anuncios</a>
-            <a href="blog.php">Blog</a>
-            <a href="contacto.php">Contacto</a>
-        </nav>
-        <p class="copyright">Todos los Derechos Reservados 2021 &copy;</p>
-    </footer>
 
-    <script src="build/js/bundle.js"></script>    
-</body>
-</html>
+<?php include 'includes/templates/footer.php';?>
