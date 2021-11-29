6e76576715c66851ce56cee67f4c8b38eb274651
diegofdg <diegofdg@gmail.com>
2021-11-28 22:42:43 -0300

    05_bienesraices-validar el formulario

--- 05_bienesraices/admin/propiedades/crear.php
+++ 05_bienesraices/admin/propiedades/crear.php
@@ -1,6 +1,8 @@
 <?php
     require '../../includes/config/database.php';
     $db = conectarDB();
+
+    $errores = [];
     
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $titulo = $_POST['titulo'];
@@ -11,9 +13,42 @@
         $estacionamiento = $_POST['estacionamiento'];
         $vendedorId = $_POST['vendedor'];
 
-        $query = " INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedorId ) VALUES ( '$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedorId' ) ";        
+        if(!$titulo) {
+            $errores[] = "Debes añadir un titulo";
+        }
+        if(!$precio) {
+            $errores[] = 'El Precio es Obligatorio';
+        }
 
-        $resultado = mysqli_query($db, $query);                
+        if( strlen( $descripcion ) < 50 ) {
+            $errores[] = 'La descripción es obligatoria y debe tener al menos 50 caracteres';
+        }
+
+        if(!$habitaciones) {
+            $errores[] = 'El Número de habitaciones es obligatorio';
+        }
+        
+        if(!$wc) {
+            $errores[] = 'El Número de Baños es obligatorio';
+        }
+
+        if(!$estacionamiento) {
+            $errores[] = 'El Número de lugares de Estacionamiento es obligatorio';
+        }
+        
+        if(!$vendedorId) {
+            $errores[] = 'Elige un vendedor';
+        }
+        
+        if(empty($errores)) {
+            $query = " INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedorId ) VALUES ( '$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedorId' ) ";        
+
+            $resultado = mysqli_query($db, $query);                
+
+            if($resultado) {
+                echo 'Insertado correctamente';
+            }
+        } 
     }
 
     require '../../includes/funciones.php';
@@ -24,6 +59,12 @@
     <main class="contenedor seccion">
         <h1>Crear</h1>
         <a href="/admin/" class="boton boton-verde">Volver</a>
+
+        <?php foreach($errores as $error): ?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+        <?php endforeach; ?>
         
         <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
             <fieldset>
@@ -76,6 +117,7 @@
             <fieldset>
                 <legend>Vendedor</legend>
                 <select name="vendedor">
+                    <option value="">-- Seleccione --</option>
                     <option value="1">Juan</option>
                     <option value="2">Karen</option>
                 </select>
