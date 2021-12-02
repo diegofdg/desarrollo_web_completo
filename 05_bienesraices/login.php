03dc654df31f47f6df3ae2797d307b9bf189bd84
diegofdg <diegofdg@gmail.com>
2021-12-01 23:39:47 -0300

    05_bienesraices-validaciones al formulario

--- 05_bienesraices/login.php
+++ 05_bienesraices/login.php
@@ -1,4 +1,22 @@
 <?php     
+    require 'includes/config/database.php';
+    $db = conectarDB();
+
+    $errores = [];
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $email = mysqli_real_escape_string($db,  filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL) );
+        $password = mysqli_real_escape_string($db,  $_POST['password']);
+
+        if(!$email) {            
+            $errores[] = "El email es obligatorio o no es válido";
+        }
+
+        if(!$password) {
+            $errores[] = "El Password es obligatorio";
+        }        
+    }
+
     require 'includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -6,15 +24,21 @@
     <main class="contenedor seccion contenido-centrado">
         <h1>Iniciar Sesión</h1>
 
-        <form class="formulario">
+        <?php foreach($errores as $error): ?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+        <?php endforeach; ?>
+
+        <form method="POST" class="formulario">
             <fieldset>
                 <legend>Email y Password</legend>
 
                 <label for="email">E-mail</label>
-                <input type="email" name="email" placeholder="Tu Email" id="email">
+                <input type="email" name="email" placeholder="Tu Email" id="email" required>
 
                 <label for="password">Password</label>
-                <input type="password" name="password" placeholder="Tu Password" id="password">
+                <input type="password" name="password" placeholder="Tu Password" id="password" required>
             </fieldset>
         
             <input type="submit" value="Iniciar Sesión" class="boton boton-verde">
