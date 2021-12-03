6298d1f32e94c65d8c08b3a24baab0b44f7a7f5d
diegofdg <diegofdg@gmail.com>
2021-12-02 23:45:49 -0300

    05_bienesraices-almacenando en la base de datos

--- 05_bienesraices/classes/Propiedad.php
+++ 05_bienesraices/classes/Propiedad.php
@@ -35,7 +35,11 @@ class Propiedad {
 
         $atributos = $this->sanitizarAtributos();         
 
-        $query = " INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId ) VALUES ( '$this->titulo', '$this->precio', '$this->imagen', '$this->descripcion', '$this->habitaciones', '$this->wc', '$this->estacionamiento', '$this->creado', '$this->vendedorId' ) ";
+        $query = " INSERT INTO propiedades ( ";
+        $query .= join(', ', array_keys($atributos));
+        $query .= " ) VALUES (' ";
+        $query .= join("', '", array_values($atributos));
+        $query .= " ') ";
         
         $resultado = self::$db->query($query);
         debuguear($resultado);
