73776767e710f7134fb1e5a1223816e77c1ea110
diegofdg <diegofdg@gmail.com>
2021-12-06 15:29:19 -0300

    05_bienesraices-modificando la clase propiedades y vendedores

--- 05_bienesraices/classes/ActiveRecord.php
+++ 05_bienesraices/classes/ActiveRecord.php
@@ -5,38 +5,13 @@ namespace App;
 class ActiveRecord {
 
     protected static $db;
-    protected static $columnasDB = ['id','titulo', 'precio', 'imagen', 'descripcion', 'habitaciones', 'wc', 'estacionamiento', 'creado', 'vendedorId'];    
+    protected static $columnasDB = [];    
     protected static $tabla = '';
 
     protected static $errores = [];
 
-    public $id;
-    public $titulo;
-    public $precio;
-    public $imagen;
-    public $descripcion;
-    public $habitaciones;
-    public $wc;
-    public $estacionamiento;
-    public $creado;
-    public $vendedorId;
-
     public static function setDB($database) {
         self::$db = $database;
-    }
-
-    public function __construct($args = []) 
-    {
-        $this->id = $args['id'] ?? null;
-        $this->titulo = $args['titulo'] ?? '';
-        $this->precio = $args['precio'] ?? '';
-        $this->imagen = $args['imagen'] ?? '';
-        $this->descripcion = $args['descripcion'] ?? '';
-        $this->habitaciones = $args['habitaciones'] ?? '';
-        $this->wc = $args['wc'] ?? '';
-        $this->estacionamiento = $args['estacionamiento'] ?? '';
-        $this->creado = date('Y/m/d');
-        $this->vendedorId = $args['vendedorId'] ?? 1;        
     }
 
     public function guardar() {
@@ -203,7 +178,7 @@ class ActiveRecord {
     }
 
     protected static function crearObjeto($registro) {
-        $objeto = new self;
+        $objeto = new static;
 
         foreach($registro as $key => $value){
             if(property_exists($objeto, $key)){
