88acd705a84e3935924544edd750535b30610e92
diegofdg <diegofdg@gmail.com>
2021-12-09 15:50:23 -0300

    05_bienesraices-eliminar propiedades

--- 05_bienesraices/controllers/PropiedadController.php
+++ 05_bienesraices/controllers/PropiedadController.php
@@ -91,4 +91,19 @@
                 'vendedores' => $vendedores
             ]);
         }
+
+        public static function eliminar() {
+            if($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $id = $_POST['id'];
+                $id = filter_var($id, FILTER_VALIDATE_INT);
+        
+                if($id) {
+                    $tipo = $_POST['tipo'];
+        
+                    $propiedad = Propiedad::find($id);            
+                    $propiedad->eliminar();
+                }        
+            }
+
+        }
     }
