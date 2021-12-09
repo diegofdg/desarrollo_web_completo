90658fd9bbc14be78ce56eed94764b17fd9a7b3c
diegofdg <diegofdg@gmail.com>
2021-12-09 19:12:14 -0300

    05_bienesraices-creando la página nosotros

--- 05_bienesraices/controllers/PaginasController.php
+++ 05_bienesraices/controllers/PaginasController.php
@@ -14,8 +14,8 @@
             ] );
         }
         
-        public static function nosotros() {
-            echo "Desde nosotros";
+        public static function nosotros(Router $router) {
+            $router->render('paginas/nosotros', []);                
         }
         
         public static function propiedades() {
