8b38446cf7a6117eff6d0f8e366bc4b17a2d9e2d
diegofdg <diegofdg@gmail.com>
2021-12-09 15:58:53 -0300

    05_bienesraices-creando el controlador para vendedores

---
+++ 05_bienesraices/controllers/VendedorController.php
@@ -0,0 +1,16 @@
+<?php 
+    namespace Controllers;
+
+    class VendedorController {
+        public static function crear() {
+            echo "Crear vendedor";
+        }
+
+        public static function actualizar() {
+            echo "Actualizar vendedor";
+        }
+
+        public static function eliminar() {
+            echo "Eliminar vendedor";
+        }
+    }
