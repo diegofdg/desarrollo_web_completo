dfda7077ba86bc0172f757938244ee6db974402b
diegofdg <diegofdg@gmail.com>
2021-12-09 15:46:31 -0300

    05_bienesraices-actualizando en la base de datos

--- 05_bienesraices/controllers/PropiedadController.php
+++ 05_bienesraices/controllers/PropiedadController.php
@@ -61,6 +61,30 @@
 
             $errores = Propiedad::getErrores();
 
+            if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+                $args = $_POST['propiedad'];
+                        
+                $propiedad->sincronizar($args);        
+        
+                $errores = $propiedad->validar();
+        
+                $nombreImagen = md5( uniqid( rand(), true ) ) . ".jpg";    
+        
+                if($_FILES['propiedad']['tmp_name']['imagen']){
+                    $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800,600);
+                    $propiedad->setImagen($nombreImagen);
+                }
+        
+                if(empty($errores)) {
+                    if($_FILES['propiedad']['tmp_name']['imagen']){
+                        $image->save(CARPETA_IMAGENES . $nombreImagen);
+                    }
+                                
+                    $propiedad->guardar();
+                }        
+            }
+
             $router->render('/propiedades/actualizar', [
                 'propiedad' => $propiedad,
                 'errores' => $errores,
