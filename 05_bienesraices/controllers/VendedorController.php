196b67182b64155bb9a922027bdb8a81efd38ab0
diegofdg <diegofdg@gmail.com>
2021-12-09 18:13:08 -0300

    05_bienesraices-eliminar vendedores

--- 05_bienesraices/controllers/VendedorController.php
+++ 05_bienesraices/controllers/VendedorController.php
@@ -52,6 +52,20 @@
         }
 
         public static function eliminar() {
-            echo "Eliminar vendedor";
+            if($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $id = $_POST['id'];
+                $id = filter_var($id, FILTER_VALIDATE_INT);
+        
+                if($id) {
+                    $tipo = $_POST['tipo'];
+        
+                    if(validadTipoContenido($tipo)) {
+                        if($tipo === 'vendedor') {
+                            $vendedor = Vendedor::find($id);            
+                            $vendedor->eliminar();
+                        } 
+                    }
+                }        
+            }
         }
     }
