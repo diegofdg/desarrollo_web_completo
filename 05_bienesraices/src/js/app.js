9f684a1506e5b496baf31eb6e6373e83e26defe3
diegofdg <diegofdg@gmail.com>
2021-12-09 23:46:55 -0300

    05_bienesraices-mostrar los campos del formulario según la selección del usuario

--- 05_bienesraices/src/js/app.js
+++ 05_bienesraices/src/js/app.js
@@ -41,6 +41,27 @@ function darkMode() {
     });
 }
 
-function mostrarMetodosContacto() {
-    console.log("seleccionando");
+function mostrarMetodosContacto(e) {
+    const contactoDiv = document.querySelector('#contacto');
+    if(e.target.value === 'telefono') {
+        contactoDiv.innerHTML = `
+            </br>
+            <label for="telefono">Número Teléfono</label>
+            <input type="tel" placeholder="Tu Teléfono" id="telefono" name="contacto[telefono]" required>
+
+            <p>Elija la fecha y la hora para la llamada</p>
+
+            <label for="fecha">Fecha:</label>
+            <input type="date" id="fecha" name="contacto[fecha]">
+
+            <label for="hora">Hora:</label>
+            <input type="time" id="hora" min="09:00" max="18:00" name="contacto[hora]">
+        `;
+    } else {
+        contactoDiv.innerHTML = `
+            </br>
+            <label for="email">E-mail</label>
+            <input type="email" placeholder="Tu Email" id="email" name="contacto[email]" required>
+        `;
+    }    
 }
