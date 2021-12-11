0c1520e152f42ddb428de33e44959a35afa59d9b
diegofdg <diegofdg@gmail.com>
2021-12-11 07:56:00 -0300

    05_bienesraices-creando el modelo de autenticación

--- 05_bienesraices/models/Admin.php
+++ 05_bienesraices/models/Admin.php
@@ -6,5 +6,16 @@ use MVC\Router;
 
 
 class Admin extends ActiveRecord {
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id','email', 'password'];
 
+    public $id;
+    public $email;
+    public $password;
+
+    public function __construct($args = []) {
+        $this->id = $args['id'] ?? null;
+        $this->email = $args['id'] ?? '';
+        $this->password = $args['id'] ?? '';
+    }
 }
