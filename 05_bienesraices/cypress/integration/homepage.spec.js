21cb61fd38b7416c3096b7c61816ca17f2c58d28
diegofdg <diegofdg@gmail.com>
2021-12-12 07:22:50 -0300

    05_bienesraices-probando la sección de iconos

--- 05_bienesraices/cypress/integration/homepage.spec.js
+++ 05_bienesraices/cypress/integration/homepage.spec.js
@@ -7,4 +7,13 @@ describe('Carga la página principal', () => {
 		cy.get('[data-cy="heading-sitio"]').invoke('text').should('equal', 'Venta de Casas y Departamentos Exclusivos de Lujo');
 		cy.get('[data-cy="heading-sitio"]').invoke('text').should('not.equal', 'Bienes Raices');
 	});
+
+	it('Prueba el Bloque de los Iconos Principales', () => {
+		cy.get('[data-cy="heading-nosotros"]').should('exist');
+		cy.get('[data-cy="heading-nosotros"]').should('have.prop', 'tagName').should('equal', 'H2');
+		
+		cy.get('[data-cy="iconos-nosotros"]').should('exist');
+		cy.get('[data-cy="iconos-nosotros"]').find('.icono').should('have.length', 3);
+		cy.get('[data-cy="iconos-nosotros"]').find('.icono').should('not.have.length', 4);
+	});
 });
