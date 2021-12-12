1ede3eb9e90f822150ba548b463e30dd0d7a7217
diegofdg <diegofdg@gmail.com>
2021-12-12 07:13:18 -0300

    05_bienesraices-comprobando que un elemento exista

--- 05_bienesraices/cypress/integration/homepage.spec.js
+++ 05_bienesraices/cypress/integration/homepage.spec.js
@@ -3,6 +3,8 @@ describe('Carga la página principal', () => {
 	it('Prueba el Header de la página principal', () => {
 		cy.visit('/');
 		
-		cy.get('[data-cy="heading-sitio"]');
+		cy.get('[data-cy="heading-sitio"]').should('exist');
+		cy.get('[data-cy="heading-sitio"]').invoke('text').should('equal', 'Venta de Casas y Departamentos Exclusivos de Lujo');
+		cy.get('[data-cy="heading-sitio"]').invoke('text').should('not.equal', 'Bienes Raices');
 	});
 });
