6f5431268e4e558e7912d9176fdb0a286221efd6
diegofdg <diegofdg@gmail.com>
2021-12-13 15:49:09 -0300

    06_appsalon-añadiendo las vistas

--- 06_appsalon/views/layout.php
+++ 06_appsalon/views/layout.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/build/img/favicon.ico" type="image/x-icon">
     <title>App Salón</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="build/css/app.css">
