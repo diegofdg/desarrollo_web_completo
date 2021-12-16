f8df49a9aeb72222b0726dc68f071d1ba56f4696
diegofdg <diegofdg@gmail.com>
2021-12-16 00:44:27 -0300

    06_appsalon-verificando si el usuario existe

--- 06_appsalon/controllers/LoginController.php
+++ 06_appsalon/controllers/LoginController.php
@@ -12,8 +12,20 @@ class LoginController {
 
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
             $auth = new Usuario($_POST);
-            $alertas = $auth->validarLogin();            
+            $alertas = $auth->validarLogin();  
+            
+            if(empty($alertas)) {                
+                $usuario = Usuario::where('email', $auth->email);
+
+                if($usuario) {                    
+                    $usuario->comprobarPasswordAndVerificado();
+                } else {
+                    Usuario::setAlerta('error', 'Usuario no encontrado');
+                }
+            }
         }
+
+        $alertas = Usuario::getAlertas();
 
         $router->render('auth/login', [
             'alertas' => $alertas
