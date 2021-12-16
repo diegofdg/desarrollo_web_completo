cfa9d6756aa383431800089768428128d5e28809
diegofdg <diegofdg@gmail.com>
2021-12-16 00:51:31 -0300

    06_appsalon-validar si el usuario está confirmado

--- 06_appsalon/controllers/LoginController.php
+++ 06_appsalon/controllers/LoginController.php
@@ -18,7 +18,9 @@ class LoginController {
                 $usuario = Usuario::where('email', $auth->email);
 
                 if($usuario) {                    
-                    $usuario->comprobarPasswordAndVerificado($auth->password);
+                    if( $usuario->comprobarPasswordAndVerificado($auth->password) ) {
+                        
+                    }
                 } else {
                     Usuario::setAlerta('error', 'Usuario no encontrado');
                 }
