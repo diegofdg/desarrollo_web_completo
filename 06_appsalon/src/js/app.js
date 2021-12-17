d94bba38be7f9d43746589891fa413158f8cd9cd
diegofdg <diegofdg@gmail.com>
2021-12-17 18:44:08 -0300

    06_appsalon-creando el paginador

--- 06_appsalon/src/js/app.js
+++ 06_appsalon/src/js/app.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function iniciarApp() {
     mostrarSeccion();
     tabs();
+    botonesPaginador();
 }
 
 function mostrarSeccion() {
@@ -34,7 +35,25 @@ function tabs() {
         boton.addEventListener('click', function(e) {
             e.preventDefault();
             paso = parseInt(e.target.dataset.paso);
+            
             mostrarSeccion();
+            botonesPaginador(); 
         });
     });
+}
+
+function botonesPaginador() {
+    const paginaAnterior = document.querySelector('#anterior');
+    const paginaSiguiente = document.querySelector('#siguiente');
+
+    if(paso === 1) {
+        paginaAnterior.classList.add('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    } else if (paso === 3) {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.add('ocultar');
+    } else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    }
 }
