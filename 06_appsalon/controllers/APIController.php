8ec9fafe8e4fa9468244921f4aaf46b7bb6b1539
diegofdg <diegofdg@gmail.com>
2021-12-20 00:09:56 -0300

    06_appsalon-almacenando las citas en postman

--- 06_appsalon/controllers/APIController.php
+++ 06_appsalon/controllers/APIController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Model\Cita;
 use Model\Servicio;
 
 class APIController {
@@ -12,11 +13,10 @@ class APIController {
     }
 
     public static function guardar() {
-        $respuesta = [
-            'datos' => $_POST
-        ];
+        $cita = new Cita($_POST);
+        $resultado = $cita->guardar();
 
-        echo json_encode($respuesta);        
+        echo json_encode($resultado);        
     }
 
     public static function eliminar() {        
