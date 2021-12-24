c62500ed21e4e8fc20a64e0cf8fafbabba2acd57
diegofdg <diegofdg@gmail.com>
2021-12-24 08:55:25 -0300

    06_appsalon-proteger el resto de las rutas

--- 06_appsalon/controllers/ServicioController.php
+++ 06_appsalon/controllers/ServicioController.php
@@ -8,6 +8,7 @@ use MVC\Router;
 class ServicioController {
     public static function index(Router $router) {    
         session_start(); 
+        isAdmin();
         
         $servicios = Servicio::all();
 
@@ -19,6 +20,7 @@ class ServicioController {
 
     public static function crear(Router $router) {
         session_start();
+        isAdmin();
 
         $servicio = new Servicio;
         $alertas = [];
@@ -43,6 +45,7 @@ class ServicioController {
 
     public static function actualizar(Router $router) {
         session_start();
+        isAdmin();
 
         if(!is_numeric($_GET['id'])) return;
 
@@ -68,7 +71,8 @@ class ServicioController {
     }
 
     public static function eliminar() {
-        session_start();        
+        session_start();
+        isAdmin();      
         
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
             $id = $_POST['id'];
