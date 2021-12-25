1c6fba32485828205136911809ed0d1f4cb5bc69
diegofdg <diegofdg@gmail.com>
2021-12-25 13:05:17 -0300

    07_uptask-comenzando el proyecto

---
+++ 07-uptask/includes/funciones.php
@@ -0,0 +1,19 @@
+<?php
+
+function debuguear($variable) : string {
+    echo "<pre>";
+    var_dump($variable);
+    echo "</pre>";
+    exit;
+}
+
+function s($html) : string {
+    $s = htmlspecialchars($html);
+    return $s;
+}
+
+function isAuth() : void {
+    if(!isset($_SESSION['login'])) {
+        header('Location: /');
+    }
+}
