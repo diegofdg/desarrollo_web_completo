c2d8593d6c636857574b492b074802ba74ed7168
diegofdg <diegofdg@gmail.com>
2021-12-26 08:18:03 -0300

    07_uptask-creando la vista para el mensaje de cuenta creada

--- 07-uptask/controllers/LoginController.php
+++ 07-uptask/controllers/LoginController.php
@@ -55,8 +55,10 @@ class LoginController {
         ]);        
     }    
 
-    public static function mensaje() {
-        echo "Desde mensaje";            
+    public static function mensaje(Router $router) {
+        $router->render('auth/mensaje', [
+            'titulo' => 'Cuenta Creada Exitosamente'
+        ]);        
         
     }    
 
