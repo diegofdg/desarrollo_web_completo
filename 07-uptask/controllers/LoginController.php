26a1eb07cb4a3307989611675249930125de5e60
diegofdg <diegofdg@gmail.com>
2021-12-26 07:00:21 -0300

    07_uptask-creando las vistas de login

--- 07-uptask/controllers/LoginController.php
+++ 07-uptask/controllers/LoginController.php
@@ -2,17 +2,33 @@
 
 namespace Controllers;
 
+use MVC\Router;
+
 class LoginController {
-    public static function login() {
-        echo "Desde login";    
+    public static function login(Router $router) {
         
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         }
+
+        $router->render('auth/login', [
+            'titulo' => 'Iniciar Sesión'
+        ]);
     }    
 
     public static function logout() {
         echo "Desde logout";
+    }    
+
+    public static function crear(Router $router) {
+        
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        }
+
+        $router->render('auth/crear', [
+            'titulo' => 'Crea tu cuenta en UpTask'
+        ]);
     }    
 
     public static function olvide() {
