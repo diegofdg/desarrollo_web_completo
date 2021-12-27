b9a900eb055758a969d76de1c96f0c15cd4356ff
diegofdg <diegofdg@gmail.com>
2021-12-27 17:07:21 -0300

    07_uptask-creando la vista de dashboard

---
+++ 07-uptask/views/templates/sidebar.php
@@ -0,0 +1,11 @@
+<aside class="sidebar">
+    <div class="contenedor-sidebar">
+        <h2>UpTask</h2>        
+    </div>    
+
+    <nav class="sidebar-nav">
+        <a href="/dashboard">Proyectos</a>
+        <a href="/crear-proyecto">Crear Proyecto</a>
+        <a href="/perfil">Perfil</a>
+    </nav>    
+</aside>
