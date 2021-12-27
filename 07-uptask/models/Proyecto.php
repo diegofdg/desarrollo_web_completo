e2f03e1034061156658501116cacb2fc6f84339e
diegofdg <diegofdg@gmail.com>
2021-12-27 19:57:31 -0300

    07_uptask-creando el modelo de proyectos

---
+++ 07-uptask/models/Proyecto.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Model;
+
+use Model\ActiveRecord;
+
+class Proyecto extends ActiveRecord {
+    protected static $tabla = 'proyectos';
+    protected static $columnasDB = ['id', 'proyecto', 'url', 'propietarioId'];
+
+    public function __construct($args = []) {
+        $this->id = $args['id'] ?? null;
+        $this->proyecto = $args['proyecto'] ?? '';
+        $this->url = $args['url'] ?? '';
+        $this->propietarioId = $args['propietarioId'] ?? '';
+    }
+}
