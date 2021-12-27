7d3ba6f70da10325e08fd1273676221f6d6baa18
diegofdg <diegofdg@gmail.com>
2021-12-26 23:40:28 -0300

    07_uptask-creando una clase para enviar un email de confirmación

--- 07-uptask/controllers/LoginController.php
+++ 07-uptask/controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Classes\Email;
 use Model\Usuario;
 use MVC\Router;
 
@@ -40,6 +41,9 @@ class LoginController {
                     unset($usuario->password2);
                     $usuario->crearToken();
                     $resultado =  $usuario->guardar();
+
+                    $email = new Email($usuario->email, $usuario->nombre, $usuario->token);
+                    $email->enviarConfirmacion();
 
                     if($resultado) {
                         header('Location: /mensaje');
