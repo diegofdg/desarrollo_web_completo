b0afd067c1a5fddb8ff97fd1492742f87a5d6b74
diegofdg <diegofdg@gmail.com>
2021-12-28 00:10:44 -0300

    07_uptask-validando el proyecto

--- 07-uptask/controllers/DashboardController.php
+++ 07-uptask/controllers/DashboardController.php
@@ -49,6 +49,25 @@ class DashboardController {
         ]);
     }
 
+    public static function proyecto(Router $router) {
+        session_start();
+
+        isAuth();
+
+        $token = $_GET['id'];
+        if(!$token) header('Location: /dashboard');
+        
+        $proyecto = Proyecto::where('url', $token);
+        
+        if($proyecto->propietarioId !== $_SESSION['id']) {
+            header('Location: /dashboard');
+        }
+
+        $router->render('dashboard/proyecto', [
+            'titulo' => $proyecto->proyecto
+        ]);
+    }
+
     public static function perfil(Router $router) {
 
         session_start();
