ef88d78f2b5e848a27bd1a929b9fc1b6440dea7f
diegofdg <diegofdg@gmail.com>
2021-12-31 00:55:36 -0300

    07_uptask-creando el modal para añadir tareas

--- 07-uptask/src/js/tareas.js
+++ 07-uptask/src/js/tareas.js
@@ -4,7 +4,32 @@
     nuevaTareaBtn.addEventListener('click', mostrarFormulario);
     
     function mostrarFormulario() {
-        console.log('Mostrando formuulario');
+        const modal = document.createElement('DIV');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+            <form class="formulario nueva-tarea">
+                <legend>Añade una nueva tarea</legend>
+                <div class="campo">
+                    <label>Tarea</label>
+                    <input 
+                        type="text"
+                        name="tarea"
+                        placeholder="Añadir Tarea al Proyecto Actual"
+                        id="tarea"                        
+                    />
+                </div>
+                <div class="opciones">
+                    <input 
+                        type="submit" 
+                        class="submit-nueva-tarea" 
+                        value="Añadir Tarea" 
+                    />
+                    <button type="button" class="cerrar-modal">Cancelar</button>
+                </div>
+            </form>
+        `;
+
+        document.querySelector('body').appendChild(modal);
     }   
 
 })();
