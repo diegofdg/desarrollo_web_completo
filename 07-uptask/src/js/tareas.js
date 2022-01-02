35ab316db40e283c59d0e0bbd948b6ce4efaa098
diegofdg <diegofdg@gmail.com>
2022-01-02 16:44:59 -0300

    07_uptask-agregando tareas a la interfaz

--- 07-uptask/src/js/tareas.js
+++ 07-uptask/src/js/tareas.js
@@ -53,8 +53,22 @@
             btnEstadoTarea.classList.add(`${estados[tarea.estado].toLowerCase()}`)
             btnEstadoTarea.textContent = estados[tarea.estado];
             btnEstadoTarea.dataset.estadoTarea = tarea.estado;
+
+            const btnEliminarTarea = document.createElement('BUTTON');
+            btnEliminarTarea.classList.add('eliminar-tarea');
+            btnEliminarTarea.dataset.idTarea = tarea.id;
+            btnEliminarTarea.textContent = 'Eliminar';
+
+            opcionesDiv.appendChild(btnEstadoTarea);
+            opcionesDiv.appendChild(btnEliminarTarea);
+
+            contenedorTarea.appendChild(nombreTarea);
+            contenedorTarea.appendChild(opcionesDiv);
+
+            const listadoTareas = document.querySelector('#listado-tareas');
+            listadoTareas.appendChild(contenedorTarea);
             
-            console.log(btnEstadoTarea);
+            
         });
     }
     
