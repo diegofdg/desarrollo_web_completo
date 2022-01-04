e0a98f6a07b64d1b521da542bb82cd462f568fb8
diegofdg <diegofdg@gmail.com>
2022-01-03 23:29:47 -0300

    07_uptask-detectando la tarea a eliminar

--- 07-uptask/src/js/tareas.js
+++ 07-uptask/src/js/tareas.js
@@ -64,6 +64,9 @@
             btnEliminarTarea.classList.add('eliminar-tarea');
             btnEliminarTarea.dataset.idTarea = tarea.id;
             btnEliminarTarea.textContent = 'Eliminar';
+            btnEliminarTarea.ondblclick = function() {
+                confirmarEliminarTarea({...tarea});
+            }
 
             opcionesDiv.appendChild(btnEstadoTarea);
             opcionesDiv.appendChild(btnEliminarTarea);
@@ -240,7 +243,30 @@
         } catch (error) {
             console.log(error);
         }
+    }
 
+    function confirmarEliminarTarea(tarea) {
+        Swal.fire({
+            title: '¿Eliminar Tarea?',
+            showCancelButton: true,
+            confirmButtonText: 'Si',
+            cancelButtonText: 'No'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                eliminarTarea(tarea);
+            } 
+        })
+    }
+
+    async function eliminarTarea(tarea) {                
+        const datos = new FormData();
+
+        try {
+            
+            
+        } catch (error) {
+            
+        }
     }
 
     function obtenerProyecto() {
