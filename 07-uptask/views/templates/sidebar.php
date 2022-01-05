7b19d23172b31071494f715b31d197179e642a3d
diegofdg <diegofdg@gmail.com>
2022-01-04 23:48:35 -0300

    07_uptask-mostrando u ocultando el menú

--- 07-uptask/views/templates/sidebar.php
+++ 07-uptask/views/templates/sidebar.php
@@ -1,7 +1,11 @@
 <aside class="sidebar">
     <div class="contenedor-sidebar">
-        <h2>UpTask</h2>        
-    </div>    
+        <h2>UpTask</h2>
+
+        <div class="cerrar-menu">
+            <img id="cerrar-menu" src="build/img/cerrar.svg" alt="imagen cerrar menu">
+        </div>
+    </div>
 
     <nav class="sidebar-nav">
         <a class="<?php echo ($titulo === 'Proyectos') ? 'activo' : ''; ?>" href="/dashboard">Proyectos</a>
