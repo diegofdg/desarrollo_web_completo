8c7d96550844ef561266c6ab42de6a1b6aa09657
diegofdg <diegofdg@gmail.com>
2022-01-05 16:23:43 -0300

    07_uptask-creando el formulario para modificar el password

--- 07-uptask/controllers/DashboardController.php
+++ 07-uptask/controllers/DashboardController.php
@@ -111,4 +111,16 @@ class DashboardController {
             'alertas' => $alertas
         ]);
     }
+
+    public static function cambiar_password(Router $router) {
+        session_start();
+        isAuth();
+
+        $alertas = [];
+
+        $router->render('dashboard/cambiar-password', [
+            'titulo' => 'Cambiar Password',
+            'alertas' => $alertas
+         ]);
+    }
 }
