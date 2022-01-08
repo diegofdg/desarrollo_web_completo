f1b1da52074e4fe5d80803bd71c7f1b8a1f0c29b
diegofdg <diegofdg@gmail.com>
2022-01-08 19:49:12 -0300

    08_gdlwebcamp-creando las variables de nuestros campos

--- 08_gdlwebcamp/js/main.js
+++ 08_gdlwebcamp/js/main.js
@@ -2,6 +2,17 @@
     "use strict";  
     
     document.addEventListener('DOMContentLoaded', function() {
-        console.log('listo');
+        var nombre = document.getElementById('nombre');
+        var apellido = document.getElementById('apellido');
+        var email = document.getElementById('email');
+
+        var pase_dia = document.getElementById('pase_dia');
+        var pase_dosdias = document.getElementById('pase_dosdias');
+        var pase_completo = document.getElementById('pase_completo');
+
+        var calcular = document.getElementById('calcular');
+        var errorDiv = document.getElementById('error');
+        var botonRegistro = document.getElementById('btnRegistro');
+        var resultado = document.getElementById('lista-productos');
     });
 })();
