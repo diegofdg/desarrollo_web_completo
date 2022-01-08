d600c353a2f75dbf6c7a05271e992646fac4ed59
diegofdg <diegofdg@gmail.com>
2022-01-08 20:43:06 -0300

    08_gdlwebcamp-cargando las liberías y mostrando un mapa

--- 08_gdlwebcamp/js/main.js
+++ 08_gdlwebcamp/js/main.js
@@ -4,6 +4,16 @@
     var regalo = document.getElementById('regalo');
     
     document.addEventListener('DOMContentLoaded', function() {
+        var map = L.map('mapa').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([51.5, -0.09]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+            
         var nombre = document.getElementById('nombre');
         var apellido = document.getElementById('apellido');
         var email = document.getElementById('email');
