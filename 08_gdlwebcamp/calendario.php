6403518f72176122c521f7a8191991610c817f93
diegofdg <diegofdg@gmail.com>
2022-01-09 18:39:43 -0300

    08_gdlwebcamp-uniendo multiples tablas con inner join

--- 08_gdlwebcamp/calendario.php
+++ 08_gdlwebcamp/calendario.php
@@ -6,7 +6,13 @@
         <?php
             try {
                 require_once('includes/funciones/bd_conexion.php');
-                $sql = 'SELECT * FROM eventos';
+                $sql = " SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado ";
+                $sql .= " FROM eventos ";
+                $sql .= " INNER JOIN categoria_evento ";
+                $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
+                $sql .= " INNER JOIN invitados ";
+                $sql .= " ON eventos.id_inv = invitados.invitado_id ";
+                $sql .= " ORDER BY evento_id ";
                 $resultado = $conn->query($sql);
 
             } catch(\Exception $e) {
