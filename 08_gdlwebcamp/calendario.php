2ad6608c91ef4619ecf59d826d18a3e3b2e46cce
diegofdg <diegofdg@gmail.com>
2022-01-09 18:27:42 -0300

    08_gdlwebcamp-consultando la base de datos con los eventos

--- 08_gdlwebcamp/calendario.php
+++ 08_gdlwebcamp/calendario.php
@@ -2,6 +2,30 @@
 
     <section class="seccion contenedor">
         <h2>Calendario de Eventos</h2>
+
+        <?php
+            try {
+                require_once('includes/funciones/bd_conexion.php');
+                $sql = 'SELECT * FROM eventos';
+                $resultado = $conn->query($sql);
+
+            } catch(\Exception $e) {
+                echo $e->getMessage();
+            }
+        ?>
+        <div class="calendario">
+            <?php
+                $eventos = $resultado->fetch_assoc();
+            ?>
+            <pre>
+                <?php var_dump($eventos); ?>
+            </pre>
+        </div>
+
+        <?php
+            $conn->close();
+        ?>
+
     </section>
 
 <?php include_once 'includes/templates/footer.php'; ?>
