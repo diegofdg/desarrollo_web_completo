65f316544b6166e8c723d9d8e516ff6fa5f2cac2
diegofdg <diegofdg@gmail.com>
2022-01-09 18:20:22 -0300

    08_gdlwebcamp-creando el archivo de conexión

---
+++ 08_gdlwebcamp/calendario.php
@@ -0,0 +1,7 @@
+<?php include_once 'includes/templates/header.php'; ?>
+
+    <section class="seccion contenedor">
+        <h2>Calendario de Eventos</h2>
+    </section>
+
+<?php include_once 'includes/templates/footer.php'; ?>
