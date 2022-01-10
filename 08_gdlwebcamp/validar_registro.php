d870d6de04d32482df3c834a3d7ea681d49adc5a
diegofdg <diegofdg@gmail.com>
2022-01-10 06:24:45 -0300

    08_gdlwebcamp-manejando las variables del formulario de registro

--- 08_gdlwebcamp/validar_registro.php
+++ 08_gdlwebcamp/validar_registro.php
@@ -2,9 +2,18 @@
 
     <section class="seccion contenedor">
         <h2>Resumen Registro</h2>
+        <?php if(isset($_POST['submit'])) : 
+            $nombre = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
+            $email = $_POST['email'];
+            $regalo = $_POST['regalo'];
+            $total = $_POST['total_pedido'];
+            $fecha = date('Y-m-d H:i:s');
+        ?>
         <pre>
             <?php var_dump($_POST); ?>
         </pre>
+        <?php endif; ?>
     </section>
 
 <?php include_once 'includes/templates/footer.php'; ?>
