41bc8e62af565624022d953557fa96f43862884e
diegofdg <diegofdg@gmail.com>
2022-01-09 23:37:32 -0300

    08_gdlwebcamp-cargando archivos de forma condicional

--- 08_gdlwebcamp/includes/templates/footer.php
+++ 08_gdlwebcamp/includes/templates/footer.php
@@ -35,7 +35,17 @@
     <script src="js/jquery.countdown.min.js"></script>
     <script src="js/jquery.lettering.js"></script>
     <script src="js/jquery.waypoints.min.js"></script>
-    <script src="js/jquery.colorbox-min.js"></script>
+    
+    <?php 
+        $archivo = basename($_SERVER['PHP_SELF']);
+        $pagina = str_replace('.php', '', $archivo);
+        if($pagina === 'invitados') {
+            echo '<script src="js/jquery.colorbox-min.js"></script>';
+        } else if($pagina === 'conferencia') {
+            echo '<script src="js/lightbox.js"></script>';
+        }
+    ?>
+    
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
     <script src="js/main.js"></script>
 
--- 08_gdlwebcamp/includes/templates/header.php
+++ 08_gdlwebcamp/includes/templates/header.php
@@ -18,7 +18,16 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
     <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/colorbox.css">
+    
+    <?php 
+        $archivo = basename($_SERVER['PHP_SELF']);
+        $pagina = str_replace('.php', '', $archivo);
+        if($pagina === 'invitados') {
+            echo '<link rel="stylesheet" href="css/colorbox.css">';
+        } else if($pagina === 'conferencia') {
+            echo '<link rel="stylesheet" href="css/lightbox.css">';
+        }
+    ?>
 
     <meta name="theme-color" content="#fafafa">
 </head>
