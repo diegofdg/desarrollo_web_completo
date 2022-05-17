eeacf1c89398df280877d1048f3f184d6b1f4a18
diegofdg <diegofdg@gmail.com>
2022-05-17 18:03:21 -0300

    06_appsalon-crear variables de entorno

--- 06_appsalon/includes/app.php
+++ 06_appsalon/includes/app.php
@@ -1,8 +1,11 @@
 <?php 
 
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
 require 'funciones.php';
 require 'database.php';
-require __DIR__ . '/../vendor/autoload.php';
 
 use Model\ActiveRecord;
 ActiveRecord::setDB($db);
