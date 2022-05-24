404e25b8546cedbb98b70501c6d83939ebd767ee
diegofdg <diegofdg@gmail.com>
2022-05-24 00:31:28 -0300

    09_paypal-rest api sdk for php deprecated-reemplazado por omnipay

--- 09_paypal/pagar.php
+++ 09_paypal/pagar.php
@@ -1,76 +1,23 @@
-<?php 
-
- if(!isset($_POST['producto'], $_POST['precio'])) {
-   exit("hubo un error");
- }
+<?php
+require_once 'config.php';
  
-use PayPal\Api\Amount;
-use PayPal\Api\Details;
-use PayPal\Api\Item;
-use PayPal\Api\ItemList;
-use PayPal\Api\Payer;
-use PayPal\Api\Payment;
-use PayPal\Api\RedirectUrls;
-use PayPal\Api\Transaction;
-
-
-require 'config.php';
+if (isset($_POST['submit'])) {    
+    try {
+        $response = $gateway->purchase(array(
+            'amount' => $_POST['precio'],
+            'currency' => PAYPAL_CURRENCY,
+            'returnUrl' => PAYPAL_RETURN_URL,
+            'cancelUrl' => PAYPAL_CANCEL_URL,
+            'producto' => $_POST['producto']
+        ))->send();
  
-$producto = $_POST['producto'];
-$precio = $_POST['precio'];
-$envio = 0;
-$total = $precio + $envio;
-
-$compra = new Payer();
-$compra->setPaymentMethod('paypal');
-
-
-$articulo = new Item();
-$articulo->setName($producto)
-      ->setCurrency('MXN')
-      ->setQuantity(1)
-      ->setPrice($precio);
-      
-      
-$listaArticulos = new ItemList();
-$listaArticulos->setItems(array($articulo));
-  
-$detalles = new Details();
-$detalles->setShipping($envio)
-          ->setSubtotal($precio); 
-          
-          
-$cantidad = new Amount();
-$cantidad->setCurrency('MXN')
-          ->setTotal($total)
-          ->setDetails($detalles);
-          
-$transaccion = new Transaction();
-$transaccion->setAmount($cantidad)
-               ->setItemList($listaArticulos)
-               ->setDescription('Pago ')
-               ->setInvoiceNumber(uniqid());
-               
-
-$redireccionar = new RedirectUrls();
-$redireccionar->setReturnUrl(URL_SITIO . "/pago_finalizado.php?exito=true")
-              ->setCancelUrl(URL_SITIO . "/pago_finalizado.php?exito=false");
-              
-              
-$pago = new Payment();
-$pago->setIntent("sale")
-     ->setPayer($compra)
-     ->setRedirectUrls($redireccionar)
-     ->setTransactions(array($transaccion));
-     
-     try {
-       $pago->create($apiContext);
-     } catch (PayPal\Exception\PayPalConnectionException $pce) {
-       // Don't spit out errors or use "exit" like this in production code
-       echo '<pre>';print_r(json_decode($pce->getData()));exit;
-   }
-
-$aprobado = $pago->getApprovalLink();
-
-
-header("Location: {$aprobado}");
+        if ($response->isRedirect()) {
+            $response->redirect(); // this will automatically forward the customer
+        } else {
+            // not successful
+            echo $response->getMessage();
+        }
+    } catch(Exception $e) {
+        echo $e->getMessage();
+    }
+}
