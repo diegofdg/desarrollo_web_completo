7bc4a6dd1a415c50479336285775600bf4838cef
diegofdg <diegofdg@gmail.com>
2022-05-25 00:57:01 -0300

    08_gdlwebcamp-agregando artículos a la compra

--- 08_gdlwebcamp/pagar.php
+++ 08_gdlwebcamp/pagar.php
@@ -11,54 +11,36 @@ if (isset($_POST['submit'])) {
     $fecha = date('Y-m-d H:i:s');
 
     $boletos = $_POST['boletos'];
+    $numero_boletos = $boletos;
     $camisas = $_POST['pedido_extra']['camisas']['cantidad'];
     $precioCamisa = $_POST['pedido_extra']['camisas']['precio'];
     
     $etiquetas = $_POST['pedido_extra']['etiquetas']['cantidad'];;
     $precioEtiquetas = $_POST['pedido_extra']['etiquetas']['precio'];;
 
+    $i=0;
+
+    foreach($numero_boletos as $key => $value) {
+        if( (int) $value['cantidad'] > 0) {
+            ${"array$i"} = array('Pase' => $key, 'Cantidad' => (int) $value['cantidad'], 'Precio' => (int) $value['precio']);               
+            $i++;
+        }
+    }
+
     echo "<pre>";
     var_dump($_POST);
     echo "</pre>";
     exit;
 
-    include_once 'includes/funciones/funciones.php';
-
-    $pedido = productos_json($boletos, $camisas, $etiquetas);
-
-    $eventos = $_POST['registro'];
-    $registro = eventos_json($eventos);
-
     try {
-        require_once('includes/funciones/bd_conexion.php');
-        $stmt = $conn->prepare("INSERT INTO registrados (nombre_registrado, apellido_registrado, email_registrado, fecha_registro, pases_articulos, talleres_registrados, regalo, total_pagado) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("ssssssis", $nombre, $apellido, $email, $fecha, $pedido, $registro, $regalo, $total);
-        $stmt->execute();
-        $stmt->close();
-        $conn->close();
-        header('Location: validar_registro.php?exitoso=1');
-    } catch(\Exception $e) {
-        echo $e->getMessage();
-    }  
-
-
-
-
-
-
-
-
-
-
-
-
-    try {
-        $response = $gateway->purchase(array(
+        $response = $gateway->purchase([
             'amount' => $_POST['precio'],
             'currency' => PAYPAL_CURRENCY,
             'returnUrl' => PAYPAL_RETURN_URL,
-            'cancelUrl' => PAYPAL_CANCEL_URL,
-            'producto' => $_POST['producto']
+            'cancelUrl' => PAYPAL_CANCEL_URL
+        ])->setItems(array(
+            array('name' => $producto, 'quantity' => 1, 'price' => $precio),
+            array('name' => 'envío gratis', 'quantity' => 1, 'price' => '0.00')
         ))->send();
  
         if ($response->isRedirect()) {
@@ -70,4 +52,6 @@ if (isset($_POST['submit'])) {
     } catch(Exception $e) {
         echo $e->getMessage();
     }
+
+
 }
