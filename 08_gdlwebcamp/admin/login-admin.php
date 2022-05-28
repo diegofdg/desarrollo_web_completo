695ec1e32196fa1cc1bdcb9473c9875367f488fe
diegofdg <diegofdg@gmail.com>
2022-05-28 20:33:03 -0300

    08_gdlwebcamp-mostrando el panel de administradores a ciertos usuarios

--- 08_gdlwebcamp/admin/login-admin.php
+++ 08_gdlwebcamp/admin/login-admin.php
@@ -8,7 +8,7 @@
             $stmt = $conn->prepare("SELECT * FROM admins WHERE usuario = ?;");
             $stmt->bind_param("s", $usuario);
             $stmt->execute();
-            $stmt->bind_result($id_admin, $usuario_admin, $nombre_admin, $password_admin, $editado);
+            $stmt->bind_result($id_admin, $usuario_admin, $nombre_admin, $password_admin, $editado, $nivel);
             if($stmt->affected_rows) {
                 $existe = $stmt->fetch();
                 if($existe) {
@@ -16,6 +16,8 @@
                         session_start();
                         $_SESSION['usuario'] = $usuario_admin;
                         $_SESSION['nombre'] = $nombre_admin;
+                        $_SESSION['nivel'] = $nivel;
+                        $_SESSION['id'] = $id_admin;
                         $respuesta = array(
                             'respuesta' => 'exitoso',
                             'usuario' => $nombre_admin
