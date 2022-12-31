2540694f4d5fe1813b1e92d451fef772191ae168
diegofdg <diegofdg@gmail.com>
2022-12-31 08:33:48 -0300

    08_devwebcamp-routing en el panel de administración

--- 08_devwebcamp/public/index.php
+++ 08_devwebcamp/public/index.php
@@ -5,6 +5,10 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\AuthController;
 use Controllers\DashboardController;
+use Controllers\PonentesController;
+use Controllers\EventosController;
+use Controllers\RegistradosController;
+use Controllers\RegalosController;
 
 $router = new Router();
 
@@ -33,5 +37,12 @@ $router->get('/confirmar-cuenta', [AuthController::class, 'confirmar']);
 // Area de administración
 $router->get('/admin/dashboard', [DashboardController::class, 'index']);
 
+$router->get('/admin/ponentes', [PonentesController::class, 'index']);
+
+$router->get('/admin/eventos', [EventosController::class, 'index']);
+
+$router->get('/admin/registrados', [RegistradosController::class, 'index']);
+
+$router->get('/admin/regalos', [RegalosController::class, 'index']);
 
 $router->comprobarRutas();
