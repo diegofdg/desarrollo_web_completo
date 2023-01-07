87362bc12ab76f0c485d05f03bfa510ccf3bde89
diegofdg <diegofdg@gmail.com>
2023-01-07 08:24:38 -0300

    08_devwebcamp-primeros pasos con el formulario

--- 08_devwebcamp/controllers/EventosController.php
+++ 08_devwebcamp/controllers/EventosController.php
@@ -10,5 +10,16 @@ class EventosController {
         $router->render('admin/eventos/index', [
             'titulo' => 'Conferencias y Workshops'
         ]);
-    }    
+    }
+
+    public static function crear(Router $router) {
+
+        $alertas = [];
+
+        $router->render('admin/eventos/crear', [
+            'titulo' => 'Registrar Evento',
+            'alertas' => $alertas
+
+        ]);
+    }
 }
