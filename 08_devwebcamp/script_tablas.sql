0945d708e1ed631835e433b37a4c2c0f1ae55748
diegofdg <diegofdg@gmail.com>
2023-01-08 09:01:51 -0300

    08_devwebcamp-creando la tabla eventos y el modelo

--- 08_devwebcamp/script_tablas.sql
+++ 08_devwebcamp/script_tablas.sql
@@ -40,6 +40,26 @@ CREATE TABLE `horas` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+CREATE TABLE `eventos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(120) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `descripcion` text,
+  `disponibles` int DEFAULT NULL,
+  `categoria_id` int NOT NULL,
+  `dia_id` int NOT NULL,
+  `hora_id` int NOT NULL,
+  `ponente_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_eventos_categorias_idx` (`categoria_id`),
+  KEY `fk_eventos_dias1_idx` (`dia_id`),
+  KEY `fk_eventos_horas1_idx` (`hora_id`),
+  KEY `fk_eventos_ponentes1_idx` (`ponente_id`),
+  CONSTRAINT `fk_eventos_categorias` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`),
+  CONSTRAINT `fk_eventos_dias1` FOREIGN KEY (`dia_id`) REFERENCES `dias` (`id`),
+  CONSTRAINT `fk_eventos_horas1` FOREIGN KEY (`hora_id`) REFERENCES `horas` (`id`),
+  CONSTRAINT `fk_eventos_ponentes1` FOREIGN KEY (`ponente_id`) REFERENCES `ponentes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
 INSERT INTO `horas` (`id`, `hora`) VALUES
 (1, '10:00 - 10:55'),
 (2, '11:00 - 11:55'),
