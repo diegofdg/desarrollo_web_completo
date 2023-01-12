2c1fcd889350dde6d825f102b0c3bb091ecb5fbc
diegofdg <diegofdg@gmail.com>
2023-01-12 07:15:49 -0300

    08_devwebcamp-añadir un ponente al workshop o conferencia-obtener ponentes

--- 08_devwebcamp/public/index.php
+++ 08_devwebcamp/public/index.php
@@ -8,6 +8,7 @@ use Controllers\DashboardController;
 use Controllers\PonentesController;
 use Controllers\EventosController;
 use Controllers\APIEventos;
+use Controllers\APIPonentes;
 use Controllers\RegistradosController;
 use Controllers\RegalosController;
 
@@ -50,6 +51,7 @@ $router->get('/admin/eventos/crear', [EventosController::class, 'crear']);
 $router->post('/admin/eventos/crear', [EventosController::class, 'crear']);
 
 $router->get('/api/eventos-horario', [APIEventos::class, 'index']);
+$router->get('/api/ponentes', [APIPonentes::class, 'index']);
 
 $router->get('/admin/registrados', [RegistradosController::class, 'index']);
 
