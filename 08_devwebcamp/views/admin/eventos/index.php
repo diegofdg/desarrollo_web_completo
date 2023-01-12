d8da11a6ddf030afa48ee5fb0d3fcf70eed879aa
diegofdg <diegofdg@gmail.com>
2023-01-12 09:56:06 -0300

    08_devwebcamp-consultando la bd para listar los eventos

--- 08_devwebcamp/views/admin/eventos/index.php
+++ 08_devwebcamp/views/admin/eventos/index.php
@@ -6,3 +6,38 @@
         Añadir Evento
     </a>
 </div>
+
+<div class="dashboard__contenedor">
+    <?php if(!empty($eventos)) { ?>
+        <table class="table">
+            <thead class="table__thead">
+                <tr>
+                    <th scope="col" class="table__th">Evento</th>
+                    <th scope="col" class="table__th">Categoría</th>
+                    <th scope="col" class="table__th">Día y Hora</th>
+                    <th scope="col" class="table__th">Ponente</th>
+                    <th scope="col" class="table__th"></th>
+                </tr>
+            </thead>
+
+            <tbody class="table__tbody">
+                <?php foreach($eventos as $evento) { ?>
+                    <tr class="table__tr">
+                        <td class="table__td">
+                            <?php echo $evento->nombre; ?>
+                        </td>
+                        <td class="table__td">
+                            <?php echo $evento->categoria_id; ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    <?php } else { ?>
+        <p class="text-center">No Hay Eventos Aún</p>
+    <?php } ?>
+</div>
+
+<?php 
+    echo $paginacion;
+?>
