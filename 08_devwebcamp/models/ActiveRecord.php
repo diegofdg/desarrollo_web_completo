f3c10418f8f9b23ba8fa063a186199374d0d2409
diegofdg <diegofdg@gmail.com>
2023-01-13 08:29:36 -0300

    08_devwebcamp-creando el bloque con el resumen y cantidades

--- 08_devwebcamp/models/ActiveRecord.php
+++ 08_devwebcamp/models/ActiveRecord.php
@@ -162,11 +162,14 @@ class ActiveRecord {
     // Traer un total de registros
     public static function total($columna = '', $valor = '') {
         $query = "SELECT COUNT(*) FROM " . static::$tabla;
+        if($columna) {
+            $query .= " WHERE ${columna} = ${valor}";
+        }
         $resultado = self::$db->query($query);
         $total = $resultado->fetch_array();
 
         return array_shift($total);
-    }
+    }   
 
     // crea un nuevo registro
     public function crear() {
