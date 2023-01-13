94c42bd2b8f17dd0f253cd31846880e6d86b9172
diegofdg <diegofdg@gmail.com>
2023-01-13 08:06:53 -0300

    08_devwebcamp-resaltar la página actual

--- 08_devwebcamp/includes/funciones.php
+++ 08_devwebcamp/includes/funciones.php
@@ -6,19 +6,23 @@ function debuguear($variable) : string {
     echo "</pre>";
     exit;
 }
+
 function s($html) : string {
     $s = htmlspecialchars($html);
     return $s;
 }
+
 function pagina_actual($path ) : bool {
     return str_contains( $_SERVER['PATH_INFO'] ?? '/', $path  ) ? true : false;
 }
+
 function is_auth() : bool {
     if(!isset($_SESSION)) {
         session_start();
     }
     return isset($_SESSION['nombre']) && !empty($_SESSION);
 }
+
 function is_admin() : bool {
     if(!isset($_SESSION)) {
         session_start();
