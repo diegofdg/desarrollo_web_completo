8db7c45539ee3304206e602c07ba0a2c7b331404
diegofdg <diegofdg@gmail.com>
2023-01-14 20:39:56 -0300

    08_devwebcamp-crear una función para añadir las animaciones

--- 08_devwebcamp/views/paginas/devwebcamp.php
+++ 08_devwebcamp/views/paginas/devwebcamp.php
@@ -3,7 +3,7 @@
     <p class="devwebcamp__descripcion">Conoce la conferencia más importante de Latinoamérica</p>
 
     <div class="devwebcamp__grid">
-        <div class="devwebcamp__imagen">
+        <div <?php aos_animacion(); ?> class="devwebcamp__imagen">
             <picture>
                 <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
                 <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
@@ -11,10 +11,10 @@
             </picture>
         </div>
 
-        <div  class="devwebcamp__contenido">
-            <p class="devwebcamp__texto">Aliquam est elit, condimentum a justo vitae, fringilla dignissim tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam quis lorem eget felis dignissim dignissim. Praesent ac lorem convallis, ultrices neque quis, cursus lorem.</p>
+        <div class="devwebcamp__contenido">
+            <p <?php aos_animacion(); ?> class="devwebcamp__texto">Aliquam est elit, condimentum a justo vitae, fringilla dignissim tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam quis lorem eget felis dignissim dignissim. Praesent ac lorem convallis, ultrices neque quis, cursus lorem.</p>
             
-            <p class="devwebcamp__texto">Aliquam est elit, condimentum a justo vitae, fringilla dignissim tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam quis lorem eget felis dignissim dignissim. Praesent ac lorem convallis, ultrices neque quis, cursus lorem.</p>
+            <p <?php aos_animacion(); ?> class="devwebcamp__texto">Aliquam est elit, condimentum a justo vitae, fringilla dignissim tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam quis lorem eget felis dignissim dignissim. Praesent ac lorem convallis, ultrices neque quis, cursus lorem.</p>
         </div>
     </div>
 </main>
