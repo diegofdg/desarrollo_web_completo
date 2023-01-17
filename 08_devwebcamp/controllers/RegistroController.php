aa2c66dbe544c226327606b6a90a4f64923e34dd
diegofdg <diegofdg@gmail.com>
2023-01-17 16:50:19 -0300

    08_devwebcamp-mostrar los eventos y lugares disponibles

--- 08_devwebcamp/controllers/RegistroController.php
+++ 08_devwebcamp/controllers/RegistroController.php
@@ -6,6 +6,11 @@ use MVC\Router;
 use Model\Paquete;
 use Model\Usuario;
 use Model\Registro;
+use Model\Dia;
+use Model\Hora;
+use Model\Evento;
+use Model\Ponente;
+use Model\Categoria;
 
 
 class RegistroController {
@@ -126,6 +131,49 @@ class RegistroController {
 
     public static function conferencias(Router $router) {
 
-        
+        if(!is_auth()) {
+            header('Location: /login');
+            return;
+        }
+
+        // Validar que el usuario tenga el plan presencial
+        $usuario_id = $_SESSION['id'];
+        $registro = Registro::where('usuario_id', $usuario_id);
+
+        if($registro->paquete_id !== "1") {
+            header('Location: /');
+            return;
+        }
+
+        $eventos = Evento::ordenar('hora_id', 'ASC');
+
+        $eventos_formateados = [];
+        foreach($eventos as $evento) {
+            $evento->categoria = Categoria::find($evento->categoria_id);
+            $evento->dia = Dia::find($evento->dia_id);
+            $evento->hora = Hora::find($evento->hora_id);
+            $evento->ponente = Ponente::find($evento->ponente_id);
+            
+            if($evento->dia_id === "1" && $evento->categoria_id === "1") {
+                $eventos_formateados['conferencias_v'][] = $evento;
+            }
+
+            if($evento->dia_id === "2" && $evento->categoria_id === "1") {
+                $eventos_formateados['conferencias_s'][] = $evento;
+            }
+
+            if($evento->dia_id === "1" && $evento->categoria_id === "2") {
+                $eventos_formateados['workshops_v'][] = $evento;
+            }
+
+            if($evento->dia_id === "2" && $evento->categoria_id === "2") {
+                $eventos_formateados['workshops_s'][] = $evento;
+            }
+        }
+
+        $router->render('registro/conferencias', [
+            'titulo' => 'Elige Workshops y Conferencias',
+            'eventos' => $eventos_formateados,
+        ]);        
     }
 }
