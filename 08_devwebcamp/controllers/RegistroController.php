c58a2658f7cfaff61d52742c1c8cb5cb3db65645
diegofdg <diegofdg@gmail.com>
2023-01-17 00:50:51 -0300

    08_devwebcamp-evitar registros duplicados

--- 08_devwebcamp/controllers/RegistroController.php
+++ 08_devwebcamp/controllers/RegistroController.php
@@ -11,6 +11,19 @@ use Model\Registro;
 class RegistroController {
 
     public static function crear(Router $router) {
+        
+        if(!is_auth()) {
+            header('Location: /');
+            return;
+        }
+
+        // Verificar si el usuario ya esta registrado
+        $registro = Registro::where('usuario_id', $_SESSION['id']);
+
+        if(isset($registro) && $registro->paquete_id === "3") {
+            header('Location: /boleto?id=' . urlencode($registro->token));
+            return;
+        }
 
         $router->render('registro/crear', [
             'titulo' => 'Finalizar Registro'
@@ -22,6 +35,14 @@ class RegistroController {
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
             if(!is_auth()) {
                 header('Location: /login');
+                return;
+            }
+
+            // Verificar si el usuario ya esta registrado
+            $registro = Registro::where('usuario_id', $_SESSION['id']);
+
+            if(isset($registro) && $registro->paquete_id === "3") {
+                header('Location: /boleto?id=' . urlencode($registro->token));
                 return;
             }
 
